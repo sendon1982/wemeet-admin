4c883164641967bd26db7328e05acd554fe9e031
ming.jiang <sendon1982@gmail.com>
2024-12-04 21:30:56 +1100

    add enrollment but not finished yet

--- src/main/java/org/wemeet/portal/config/SecurityConfiguration.java
+++ src/main/java/org/wemeet/portal/config/SecurityConfiguration.java
@@ -75,6 +75,7 @@ public class SecurityConfiguration {
                 authz
                     .requestMatchers(mvc.pattern("/index.html"), mvc.pattern("/*.js"), mvc.pattern("/*.txt"), mvc.pattern("/*.json"), mvc.pattern("/*.map"), mvc.pattern("/*.css")).permitAll()
                     .requestMatchers(mvc.pattern("/*.ico"), mvc.pattern("/*.png"), mvc.pattern("/*.svg"), mvc.pattern("/*.webapp")).permitAll()
+                    .requestMatchers(mvc.pattern("/~@/**")).permitAll()
                     .requestMatchers(mvc.pattern("/app/**")).permitAll()
                     .requestMatchers(mvc.pattern("/i18n/**")).permitAll()
                     .requestMatchers(mvc.pattern("/content/**")).permitAll()
